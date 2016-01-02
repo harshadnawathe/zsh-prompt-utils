16653195760786c98b24ccc8f9e5654f207cae59
Harshad Nawathe <harshad.nawathe@gmail.com>
2016-01-02 11:09:30 +0000

    Replaces battery status formatting functions to file

--- battery-status-functions.sh
+++ battery-status-functions.sh
@@ -8,4 +8,25 @@ function external_power_connected {
   $(uname)/external-power-connected.sh
 }
 
+function battery_percentage_colored {
+  BATTERY_PERC=$(battery_percentage)
+  if   [[ $(bc <<< "$BATTERY_PERC >= 75") -eq 1 ]]; then
+    echo "$GREEN$BATTERY_PERC%%$COLOR_RESET"
+  elif [[ $(bc <<< "$BATTERY_PERC >= 50") -eq 1 ]]; then
+    echo "$YELLOW$BATTERY_PERC%%$COLOR_RESET"
+  elif [[ $(bc <<< "$BATTERY_PERC >= 25") -eq 1 ]]; then
+    echo "$ORANGE$BATTERY_PERC%%$COLOR_RESET"
+  else
+    echo "$RED$BATTERY_PERC%%$COLOR_RESET"
+  fi
+}
+
+function charging_status {
+  if [[ $(external_power_connected) -eq 1 ]]; then
+    echo '\033[48;5;118m\xF0\x9F\x94\x8C \033[m'
+  else
+    echo '\033[48;5;196m\xF0\x9F\x94\x8B \033[m'
+  fi
+}
+
 
--- set-prompt.sh
+++ set-prompt.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 source colors-definations.sh
+
 source battery-status-functions.sh
 
 function is_git_repo {
@@ -25,27 +26,6 @@ function git_deleted_files_count {
 
 function git_staged_files_count {
   git status --porcelain 2> /dev/null | grep -e"^[ADM]." | wc -l
-}
-
-function battery_percentage_colored {
-  BATTERY_PERC=$(battery_percentage)
-  if   [[ $(bc <<< "$BATTERY_PERC >= 75") -eq 1 ]]; then
-    echo "$GREEN$BATTERY_PERC%%$COLOR_RESET"
-  elif [[ $(bc <<< "$BATTERY_PERC >= 50") -eq 1 ]]; then
-    echo "$YELLOW$BATTERY_PERC%%$COLOR_RESET"
-  elif [[ $(bc <<< "$BATTERY_PERC >= 25") -eq 1 ]]; then
-    echo "$ORANGE$BATTERY_PERC%%$COLOR_RESET"
-  else
-    echo "$RED$BATTERY_PERC%%$COLOR_RESET"
-  fi
-}
-
-function charging_status {
-  if [[ $(external_power_connected) -eq 1 ]]; then
-    echo '\033[48;5;118m\xF0\x9F\x94\x8C \033[m'
-  else
-    echo '\033[48;5;196m\xF0\x9F\x94\x8B \033[m'
-  fi
 }
 
 function modified_files_status {
