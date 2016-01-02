ba4c168ffb9448dc8b79157c4495ea2afd064282
Harshad Nawathe <harshad.nawathe@gmail.com>
2016-01-02 11:13:59 +0000

    Removes battery status function

--- set-prompt.sh
+++ set-prompt.sh
@@ -4,7 +4,7 @@ source colors-definations.sh
 
 source battery-status-functions.sh
 
-
+source git-status-functions.sh
 
 function battery_status {
  echo "$(charging_status)$(battery_percentage_colored)"
