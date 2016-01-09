e329cbd5f90a5295686133170098b5afa6942852
Harshad Nawathe <harshad.nawathe@gmail.com>
2016-01-09 17:19:14 +0000

    Replaces git status shell script with Python

---
+++ parse-and-endcode-git-status.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+import re
+from functools import reduce
+from operator import add
+from sys import stdin
+
+def color_s(c,s):
+	return '\033[38;5;{}m{}\033[m'.format(c,s)
+
+def format_s(s,n):
+	return '{}{} '.format(s,n)
+
+def format_info(color_fn, colors, format_fn, symbols, info_tup):
+	return ''.join(map(color_fn, colors, map(format_fn, symbols, info_tup)))
+
+def file_counts(git_status_txt):
+	match_files = re.compile(r'(^[ADM].\s.*$)|(^.M\s.*$)|(^\sD\s.*$)|(^\?\?\s.*$)', re.MULTILINE)
+	str_tr = lambda s: 0 if len(s) is 0 else 1
+	tuple_tr = lambda t: map(str_tr,t)
+	tuple_add = lambda t1,t2: map(add,t1,t2)
+	return reduce(tuple_add, map(tuple_tr, re.findall(match_files, git_status_txt)),(0,0,0,0))
+
+def repo_status(file_counts):
+	if sum(file_counts) == 0:
+		return color_s(40,'✓')
+	format_if_not_empty = lambda s,n: format_s(s,n) if n > 0 else ''
+	color_if_not_empty = lambda c,s: color_s(c,s) if len(s) > 0 else ''
+	return format_info(color_if_not_empty, (40,208,196,226), format_if_not_empty,('●','○','✗','?'), file_counts) 
+
+def branch_info(git_status_txt):
+	match_branch_status = re.compile(r'##\s(.*)\.\.\.(?:.*)(?:\[(?:ahead (\d+))?[,\s]{0,2}(?:behind (\d+))?\])?')
+	match = re.search(match_branch_status, git_status_txt)
+	if match is not None:
+		return match.groups()
+	else:
+		return (None,None,None)
+
+def branch_status(branch_info):
+	color_if_not_None = lambda c,s: '' if s is None else color_s(c,s)
+	format_if_not_None = lambda s,d: None if d is None else format_s(s,d)
+	return format_info(color_if_not_None, (40,226,196), format_if_not_None, ('','↾','⇂'), branch_info)
+
+if __name__ == '__main__':
+	txt = stdin.read()
+	bs = branch_status(branch_info(txt))
+	if len(bs) > 0:
+		rs = repo_status(file_counts(txt))
+		print('(%s|%s)' % (bs,rs))
--- set-prompt.sh
+++ set-prompt.sh
@@ -2,7 +2,11 @@
 
 source $ZSH_PROMPT_UTILS_ROOT/colors-definations.sh
 source $ZSH_PROMPT_UTILS_ROOT/battery-status-functions.sh
-source $ZSH_PROMPT_UTILS_ROOT/git-status-functions.sh
+#source $ZSH_PROMPT_UTILS_ROOT/git-status-functions.sh
+
+function git_status {
+	git status -b --porcelain 2> /dev/null | $ZSH_PROMPT_UTILS_ROOT/parse-and-endcode-git-status.py	
+}
 
 setopt promptsubst
 export PS1='$(battery_status) [%1d] $(git_status) ~> '
